ccd5383a88f344fbe24e686e8ad9f590ed209da1
Caleb <jiale.hou@my.jcu.edu.au>
2021-12-25 13:45:38 +0800

    programming_language.py and language.py is complete.

--- programming_language.py
+++ programming_language.py
@@ -1,6 +1,6 @@
 class ProgrammingLanguage:
 
-    def __init__(self, field="", typing="", reflection=True, year=0):
+    def __init__(self, field, typing, reflection, year):
         self.field = field
         self.typing = typing
         self.reflection = reflection
@@ -8,3 +8,6 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
